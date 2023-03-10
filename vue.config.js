49fc09c95d821c636148e2d2bca60d34d5e8caa2
Kengo Nakamura <kengonc98developer@gmail.com>
2023-03-09 20:23:44 -0400

    Fix bug in the package

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => {
+        if (options === undefined || options === null) {
+          options = {};
+        }
+        options.limit = 10240;
+        return options;
+      });
+  }
 })
